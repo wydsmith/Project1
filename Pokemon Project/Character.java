d96cc22eca4e680e1a3ddbba957229ff78105761
wydsmith <minecraftsteve767@gmail.com>
2024-11-05 11:58:59 -0500

    Add files via upload

---
+++ Pokemon Project/Character.java
@@ -0,0 +1,10 @@
+public class Character {
+    private String name;
+    
+    Character(String name){
+        this.name = name;
+    }
+    public String getName(){
+        return name;
+    }
+}
---
+++ Pokemon Project/Race.java
@@ -0,0 +1,8 @@
+public class Race extends Character {
+    
+    public Race(String name){
+        super(name);
+    }
+
+    
+}
